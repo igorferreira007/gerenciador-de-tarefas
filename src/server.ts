d024655486d9f1d536110265212a84d53c68012f
igorferreira007 <ferreiraigor007@gmail.com>
2025-05-24 09:20:02 -0300

    Adicionado e configurado o Cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import "express-async-errors"
 import { router } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
@@ -7,6 +8,7 @@ import { env } from "./env"
 const PORT = env.PORT
 const app = express()
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 app.use(express.json())
 app.use(router)
 app.use(errorHandling)
