493921c2e3a86cc18d6a0efb71479304ebbd545f
igorferreira007 <ferreiraigor007@gmail.com>
2025-06-08 12:00:11 -0300

    Ajustado a resposta do tasks-logs-controller, para retornar o titulo da tarefa

--- src/controllers/tasks-logs-controller.ts
+++ src/controllers/tasks-logs-controller.ts
@@ -6,42 +6,49 @@ import z from "zod"
 export class TasksLogsController {
   async index(request: Request, response: Response) {
     const querySchema = z.object({
-      taskId: z.string().uuid().optional()
+      taskId: z.string().uuid().optional(),
     })
 
     const userSchema = z.object({
       role: z.enum(["admin", "member"]),
-      teamId: z.string().uuid().optional()
+      teamId: z.string().uuid().optional(),
     })
 
     const { taskId } = querySchema.parse(request.query)
     const { role, teamId } = userSchema.parse(request.user)
-    
+
     const taskHistory = await prisma.taskHistory.findMany({
       where: {
         taskId: {
           contains: taskId?.toString().trim(),
-          mode: "insensitive"
-        }
+          mode: "insensitive",
+        },
       },
       include: {
         user: {
           select: {
-            name: true
-          }
-        }
+            name: true,
+          },
+        },
+        task: {
+          select: {
+            title: true,
+          },
+        },
       },
       orderBy: {
-        changedAt: "desc"
-      }
+        changedAt: "desc",
+      },
     })
 
     if (role !== "admin") {
       const tasks = await prisma.task.findMany({ where: { teamId } })
 
-      const taskId = tasks.map(task => task.id)
+      const taskId = tasks.map((task) => task.id)
 
-      const teamTaskHistories = taskHistory.filter(taskHistory => taskId.includes(taskHistory.taskId))
+      const teamTaskHistories = taskHistory.filter((taskHistory) =>
+        taskId.includes(taskHistory.taskId)
+      )
 
       return response.json(teamTaskHistories)
     }
